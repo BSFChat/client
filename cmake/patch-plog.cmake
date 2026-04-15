24d64e55efe1c83f3d874fea284306a8d9cc4717
Josh Griffith <josh@joshgriffith.co.uk>
2026-04-15 14:01:40 +0100

    CI: patch all libdatachannel vendored deps for CMake 3.31+

--- cmake/patch-plog.cmake
+++ cmake/patch-plog.cmake
@@ -1,10 +1,14 @@
-# Patch plog's outdated cmake_minimum_required for CMake 3.31+ compatibility
-file(GLOB_RECURSE _plog_files "deps/plog/CMakeLists.txt")
-foreach(_file ${_plog_files})
+# Patch outdated cmake_minimum_required in libdatachannel's vendored dependencies
+# for CMake 3.31+ compatibility (plog, usrsctp, libjuice, libsrtp)
+file(GLOB_RECURSE _cmake_files "deps/*/CMakeLists.txt")
+foreach(_file ${_cmake_files})
     file(READ "${_file}" _content)
-    string(REGEX REPLACE
-        "cmake_minimum_required\\(VERSION [0-9.]+"
-        "cmake_minimum_required(VERSION 3.5"
-        _content "${_content}")
-    file(WRITE "${_file}" "${_content}")
+    if(_content MATCHES "cmake_minimum_required\\(VERSION [0-2]\\.")
+        string(REGEX REPLACE
+            "cmake_minimum_required\\(VERSION [0-9.]+"
+            "cmake_minimum_required(VERSION 3.5"
+            _content "${_content}")
+        file(WRITE "${_file}" "${_content}")
+        message(STATUS "Patched cmake_minimum_required in ${_file}")
+    endif()
 endforeach()
